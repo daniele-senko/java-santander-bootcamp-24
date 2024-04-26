dc58cafc8737f5a7be064bdb4e94a296bd28f978
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-04-26 19:14:20 +0000

    aula 2 - anatomia das classes

---
+++ sintaxe/src/anatomia/anatomia-2/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+    public static void main (String [] args){
+       //palavra final é reservada para manter a variável imutavel
+       //palavras MAIUSCULAS geralmente são usadas como imutaveis
+        @SuppressWarnings("unused")
+        final String BR = "Brasil";
+    }
+}
