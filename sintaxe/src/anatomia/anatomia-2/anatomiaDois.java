ec4ab1d7ada2cfaf41400238725f40c4e1fa6095
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-04-26 19:44:36 +0000

    aula 3 - anatomia das classes

--- sintaxe/src/anatomia/anatomia-2/anatomiaDois.java
+++ sintaxe/src/anatomia/anatomia-2/anatomiaDois.java
@@ -1,4 +1,4 @@
-public class MinhaClasse {
+public class anatomiaDois {
     public static void main (String [] args){
        //palavra final é reservada para manter a variável imutavel
        //palavras MAIUSCULAS geralmente são usadas como imutaveis
