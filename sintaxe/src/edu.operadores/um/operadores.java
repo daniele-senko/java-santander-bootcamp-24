2c383595ac8b21797e44bb88e69e9c11ea6dcc18
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-04-29 13:40:55 +0000

    aula operadores - 2

---
+++ sintaxe/src/edu.operadores/um/operadores.java
@@ -0,0 +1,46 @@
+import java.util.Date;
+
+public class operadores {
+    public static void main(String[] args) {
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20 / 4);
+        // ATENÇÃO! O operador de adição (+), quando utilizado em variáveis do tipo
+        // texto, realizará a “concatenação de textos”.
+        // classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        // qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
+    }
+}
