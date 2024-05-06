5a46455ab889ec2e784ce26acbbce92ed209b234
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-05-06 12:52:59 +0000

    aula de for em arrays

---
+++ java-dio/src/EstruturaRepeticao/ForArrays/OutroExemploForArray.java
@@ -0,0 +1,8 @@
+public class OutroExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+        for (String aluno : alunos) {
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
